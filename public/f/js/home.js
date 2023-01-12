fd6ee5faa6f2c782a779d3632eda60f90a874bb4
Gilang Aryadi Mahardika <74519315+marchites@users.noreply.github.com>
2023-01-12 11:57:32 +0700

    [v.0.0.24] End User - Hidden navbar while scroll

--- public/f/js/home.js
+++ public/f/js/home.js
@@ -1,10 +1,11 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").classList.remove('show');
-  }
-  else { document.getElementById("header").classList.add('show');
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
